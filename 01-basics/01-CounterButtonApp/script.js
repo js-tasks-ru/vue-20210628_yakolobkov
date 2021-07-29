cf43c08e9adaeeab9889f625fd55c92ee7856dfa
Yaroslav Kolobkov <yakolobkov@paysend.com>
2021-07-29 03:37:50 +0300

    добавление экземпляра компонента

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,24 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
+
+const RootComponentOptions = defineComponent({
+  name: 'Root',
+
+  data() {
+    return {
+      a: 0,
+      hello: 'world',
+    };
+  },
+  computed: {
+    plus() {
+      return this.a + 1;
+    },
+  },
+  template: `<div> <button type="button" @click="plus">{{ a }}</button></div>`,
+});
 
 // Создайте Vue приложение
+
+const app = createApp(RootComponentOptions);
+const vm = app.mount('#app');
+console.log(vm.a);
