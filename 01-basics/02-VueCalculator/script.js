f6ce937f81657279c4dcc0332ec8d6591ae8358b
Yaroslav Kolobkov <yakolobkov@paysend.com>
2021-07-31 18:41:37 +0300

    calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,67 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const RootComponentOptions = defineComponent({
+  name: 'Root',
+
+  data() {
+    return {
+      a: 1,
+      b: 2,
+      c: 3,
+      operand: 'sum',
+      sum: 'sum',
+      subtract: 'subtract',
+      multiply: 'multiply',
+      divide: 'divide',
+    };
+  },
+  computed: {
+    FirstNumber() {
+      return this.a;
+    },
+    SecondNumber() {
+      return this.b;
+    }
+  },
+  methods: {
+    showOperand() {
+      return this.operand;
+    },
+    mathOperation() {
+      this.FirstNumber;
+      this.SecondNumber;
+      if (this.operand === 'sum') {
+        return (this.c = this.a + this.b);
+      } else if (this.operand === 'subtract') {
+        return (this.c = this.a - this.b);
+      } else if (this.operand === 'multiply') {
+        return (this.c = this.a * this.b);
+      } else {
+        return (this.c = this.a / this.b);
+      }
+    },
+  },
+  template: `<div class="row">
+    <div class="col">
+      <input v-model="a" type="number" />
+    </div>
+
+    <div class="col" style="display: flex; flex-direction: column; font-family: emoji">
+      <label for="sum"><input v-model="operand" type="radio" id="sum" :value="sum" /> ➕</label>
+      <label for="subtract"><input v-model="operand" type="radio" id="subtract" :value="subtract" /> ➖</label>
+      <label for="multiply"><input v-model="operand" type="radio" id="multiply" :value="multiply" /> ✖</label>
+      <label for="divide"><input v-model="operand" type="radio" id="divide" :value="divide" /> ➗</label>
+    </div>
+
+    <div class="col">
+      <input v-model="b" type="number" />
+    </div>
+
+    <div class="col">=</div>
+
+    <output class="col">{{ mathOperation() }}</output>
+  </div>`,
+});
+
+const app = createApp(RootComponentOptions);
+const vm = app.mount('#app');
